ffd232b94034dbc44a6be4e8b2b2f4c2393cd7cd
Irina Romjalis <iromjalis@gmail.com>
2021-07-17 23:14:25 +0200

    package.json homepage

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Switch, Route, Redirect } from "react-router-dom";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App-header">
+      <Link
+        to={{
+          pathname: "/books",
+          search: "?category=adventure",
+          hash: "#treasure-island",
+          state: { from: "/dashboard" },
+        }}
+      />
+
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/contact" component={Contact} />
+        <Redirect to="/" />
+      </Switch>
     </div>
   );
-}
+};
 
 export default App;
